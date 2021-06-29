24a97f29196955a41f526e10c00a6cd6c08f3fa4
mariamalshammari <malshammari37@gmail.com>
2021-06-29 13:02:07 +0300

    lab01finished

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+
+      <div>
+        <p>Author : &copy; Mariam Saber AL-Shammari </p>
+      </div>
+    );
+  }
+}
+export default Footer;
