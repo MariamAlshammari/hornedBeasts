f02d7f5137e8248ff1a8c1e0db762004f5b5c5ae
mariamalshammari <malshammari37@gmail.com>
2021-06-29 13:24:02 +0300

    lab02finished

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button className="btn">Learn more </button>
+        )
+    }
+}
+
+export default Button;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,25 @@
 import React from 'react'
 import HornedBeast from './HornedBeast'
-
+import Horned from './Horned.json';
+// import './Main.css';
 
 class Main extends React.Component {
     render() {
-        let hornedBeast = [
-            {
-                title:"UniWhal",
-                image_url: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-
-                description:"A unicorn and a narwhal nuzzling their horns"
-        
-            },
-            {
-                title:"Rhino Family",
-            image_url: "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-                description:"Mother (or father) rhino with two babies"
-            },
-            {
-                title:"Unicorn Head",
-            image_url: "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-                description:"Someone wearing a creepy unicorn head mask"
-            }
-        ]
-
+  
         return (
-            <div>
-                <HornedBeast title={hornedBeast[0].title} description={hornedBeast[0].description} image_url={hornedBeast[0].image_url}/>
-         <HornedBeast title={hornedBeast[1].title} description={hornedBeast[1].description} image_url={hornedBeast[1].image_url}/>  
-         <HornedBeast title={hornedBeast[2].title} description={hornedBeast[2].description} image_url={hornedBeast[2].image_url}/>
-            </div>
+            <>
+                {Horned.map((item,idx)=>{
+
+                    return (
+                    <HornedBeast title={item.title} description={item.description} image_url={item.image_url} key={idx} idx={idx}></HornedBeast>
+                    )
+
+
+                })}
+               
+            </>
         )
+        
     }
 }
 
